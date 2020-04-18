1862122d8fa54bd0e61fa1d221131c879271d155
van dung <vandung030701@gmail.com>
2020-04-18 19:33:25 +0700

    em nop bai tap 1.6

---
+++ src/javacore/buoi01/activity06.java
@@ -0,0 +1,15 @@
+package javacore.buoi01;
+import java.util.Scanner;
+public class activity06 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a;
+        System.out.println("nhap so a la:");
+        a=sc.nextInt();
+        int b;
+        System.out.println("nhap so b la:");
+        b=sc.nextInt();
+        System.out.println(a+"+"+b+ "=" +(a+b) );
+
+    }
+}
