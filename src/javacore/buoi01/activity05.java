555b1a176684ef983a2344c8e628b487c9ed6407
van dung <vandung030701@gmail.com>
2020-04-18 22:16:18 +0700

    em nop bai tap 1.5 a

--- src/javacore/buoi01/activity05.java
+++ src/javacore/buoi01/activity05.java
@@ -6,6 +6,6 @@ public class activity05 {
         int n;
         System.out.println("nhap vao gia tri cua n la:");
         n=sc.nextInt();
-        System.out.println(n+"+"+n+n+"+"+n+n+n+ "="+(n*(1+11+111)));
+        System.out.println(n+"+"+n +n +"+"+n+n+n+ "="+(n*(1+11+111)));
     }
 }
