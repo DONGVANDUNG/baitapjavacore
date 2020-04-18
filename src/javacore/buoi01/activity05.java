e810fcdff68e006553e910b23e6d071ab5a2ecdb
van dung <vandung030701@gmail.com>
2020-04-18 20:22:49 +0700

    e nop bai 1.6 a

---
+++ src/javacore/buoi01/activity05.java
@@ -0,0 +1,11 @@
+package javacore.buoi01;
+import java.util.Scanner;
+public class activity05 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        System.out.println("nhap vao gia tri cua n la:");
+        n=sc.nextInt();
+        System.out.println(n+"+"+n+n+"+"+n+n+n+ "="+(n*(1+11+111)));
+    }
+}
