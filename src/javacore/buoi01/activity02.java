4f3a9b0705d4762028f244ff8beecc5de0357188
van dung <vandung030701@gmail.com>
2020-04-18 12:51:17 +0700

    e gui ah bai tap 1.2 a

---
+++ src/javacore/buoi01/activity02.java
@@ -0,0 +1,15 @@
+package javacore.buoi01;
+import java.util.Scanner;
+public class activity02 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a;
+        int b;
+        System.out.println("nhap so a la:");
+        a=sc.nextInt();
+        System.out.println("nhap so b la:");
+        b=sc.nextInt();
+        System.out.println("tong cua a va b la:"+ (a +b ));
+        System.out.println("tich cua hai so a va b la:"+ (a+b));
+    }
+}
