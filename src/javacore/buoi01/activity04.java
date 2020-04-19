c8bc1d257c9c57eda7d0497ace932234e405b31c
van dung <vandung030701@gmail.com>
2020-04-19 10:08:08 +0700

    e nop bai tap 1.4 su dung vong lap for la:

--- src/javacore/buoi01/activity04.java
+++ src/javacore/buoi01/activity04.java
@@ -3,17 +3,17 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 public class activity04 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-         int a;
-        System.out.println("ban thich nhap a la gi nao:");
-        a=sc.nextInt();
-        System.out.println("ax1=:" + (a*1));
-        System.out.println("ax2:=" + (a*2));
-        System.out.println("ax3:=" + (a*3));
-        System.out.println("ax4:=" + (a*4));
-        System.out.println("ax5:=" + (a*5));
-        System.out.println("ax6:=" + (a*6));
-        System.out.println("ax7:=" + (a*7));
-        System.out.println("ax8:=" + (a*8));
+        Scanner sc = new Scanner(System.in);
+        int a;
+        System.out.println("hay nhap so cua ban thich vao day:");
+        a = sc.nextInt();
+        for (int i = 1; i < 10; i++){
+            int result=a*i;
+            System.out.println(a+"x"+i+ "="+(result));
+        }
+
+
     }
 }
+
+
