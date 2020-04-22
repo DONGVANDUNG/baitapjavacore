0e26b7beab6992681266adca982a3b43a39bb31a
van dung <vandung030701@gmail.com>
2020-04-22 17:16:52 +0700

    em lam mai moi ra bai 2.1 nay

---
+++ src/javacore/buoi02/com/activity21.java
@@ -0,0 +1,12 @@
+package javacore.buoi02.com;
+import java.util.Scanner;
+public class activity21 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("nhap so ban thich:=");
+        int a=sc.nextInt();
+        for (int i = 1; i <a ; i++) {
+
+        }
+    }
+}
