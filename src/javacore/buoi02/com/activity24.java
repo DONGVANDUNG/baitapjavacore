b2efbc521478cfc859a92d32b30961dac588ac8d
van dung <vandung030701@gmail.com>
2020-04-22 09:31:16 +0700

    em nop bai tap 2.4

---
+++ src/javacore/buoi02/com/activity24.java
@@ -0,0 +1,16 @@
+package javacore.buoi02.com;
+import java.util.Scanner;
+public class activity24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap so dau tien");
+        int a = sc.nextInt();
+        System.out.println("nhap so thu hai");
+        int b = sc.nextInt();
+        int s = 0;
+        for (int i = 1; i <= b; i++) {
+            s = s + a;
+        }
+        System.out.println("tong la:" + s);
+    }
+}
