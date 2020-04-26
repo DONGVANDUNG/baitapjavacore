ec83c2fb62b4404a6674eb24f6a60ad1f6de2e07
van dung <vandung030701@gmail.com>
2020-04-26 07:50:25 +0700

    em nop bai 3.4 a

--- src/javacore/buoi03/com/activity34.java
+++ src/javacore/buoi03/com/activity34.java
@@ -1,4 +1,17 @@
 package javacore.buoi03.com;
 
+import java.util.Scanner;
+
 public class activity34 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a;
+        System.out.println("nhap vao noi dung cua chuoi");
+        a = sc.nextLine();
+        char b[] = a.toCharArray();
+        for (int i = b.length - 1; i > 0; i--) {
+            System.out.print(b[i]);
+        }
+    }
 }
+
