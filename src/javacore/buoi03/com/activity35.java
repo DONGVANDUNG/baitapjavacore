dd5895d41a33fcc87b9f9386fb0abdc037477dc8
van dung <vandung030701@gmail.com>
2020-04-29 07:34:32 +0700

    em lam lai bai 3.5 a

--- src/javacore/buoi03/com/activity35.java
+++ src/javacore/buoi03/com/activity35.java
@@ -9,21 +9,21 @@ public class activity35 {
         System.out.println("nhap vap noi dung cua chuoi:");
         a = sc.nextLine();
         char[] c = a.toCharArray();
-        int max=0;
-        char d=0;
+        int max = 0;
+        char d = 0;
         int dem = 1;
         for (int i = 0; i < a.length(); i++) {
-            dem=1;
-            for(int j=i+1;j<a.length();j++){
-                if (c[i]==c[j]){
-                    dem=dem+1;
+            dem = 1;
+            for (int j = i + 1; j < a.length(); j++) {
+                if (c[i] == c[j]) {
+                    dem = dem + 1;
                 }
             }
-            if(dem>max){
-                max=dem;
-                d=c[i];
+            if (dem > max) {
+                max = dem;
+                d = c[i];
             }
-            }
-        System.out.println((d)+"("+max+")");
         }
+        System.out.println((d) + "(" + max + ")");
     }
+}
