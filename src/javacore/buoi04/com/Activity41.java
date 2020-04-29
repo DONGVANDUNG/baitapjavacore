dc71c5eef37d3b5f0529380296aaa39150ba348f
van dung <vandung030701@gmail.com>
2020-04-29 14:14:20 +0700

    em nop bai 4.1 a

--- src/javacore/buoi04/com/Activity41.java
+++ src/javacore/buoi04/com/Activity41.java
@@ -1,22 +1,20 @@
 package javacore.buoi04.com;
+
+import java.util.Scanner;
+
 public class Activity41 {
-    float width;
-    float height;
-
-    public float getWidth() {
-        return width;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("chieu dai cua hinh chu nhat la: ");
+        float a = sc.nextFloat();
+        System.out.print("chieu rong cua hinh chu nhat la: ");
+        float b = sc.nextFloat();
+        Hinhchunhat hinhchunhat = new Hinhchunhat(a, b);
+        System.out.println("chu vi hinh chu nhat la: " + hinhchunhat.getchuvi());
+        System.out.println("dien hinh chu nhat la: " + hinhchunhat.getDientich());
+        System.out.println("chieu dai la: " + hinhchunhat.getHeight());
+        System.out.println("chieu rong la: " + hinhchunhat.getWidth());
+        System.out.println(hinhchunhat.check() ? "day la hinh vuong" : "day khong la hinh vuong");
     }
 
-    public void setHeight(float height) {
-        this.height = height;
-    }
-
-    public void setWidth(float width) {
-        this.width = width;
-    }
-
-    public void hinhchunhat(float width, float height) {
-        this.width = width;
-        this.height = height;
-    }
 }
---
+++ src/javacore/buoi04/com/Hinhchunhat.java
@@ -0,0 +1,41 @@
+package javacore.buoi04.com;
+
+import java.util.Scanner;
+
+public class Hinhchunhat {
+    float width;
+    float height;
+Scanner sc=new Scanner(System.in);
+    public Hinhchunhat(float width, float height) {
+        this.width = width;
+        this.height = height;
+    }
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+    public float getchuvi(){
+        return (width+height)*2;
+    }
+    public float getDientich(){
+        return width*height;
+    }
+    public boolean check() {
+        if (height == width) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
